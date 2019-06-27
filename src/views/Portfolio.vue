56882005237d22a3c941c86d55eb27524cfa32e8
james2mid <jamesmiddleton1@hotmail.co.uk>
2019-06-27 23:17:23 +0100

    Improved placeholders

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="portfolio">
     <div style="height: 100vh; display: flex; justify-content: center; align-items: center">
-      <h1 class="is-size-1 title">Portfolio Placeholder</h1>
+      <h1 class="is-size-1 title">Portfolio</h1>
     </div>
   </div>
 </template>
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="services">
-    <h1>Services</h1>
+    <div style="height: 100vh; display: flex; justify-content: center; align-items: center">
+      <h1 class="is-size-1 title">Services</h1>
+    </div>
   </div>
 </template>
