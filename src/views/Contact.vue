6d31269ef76fec56781083a77abe5cae933da0fd
james2mid <jamesmiddleton1@hotmail.co.uk>
2019-06-27 23:16:32 +0100

    Finished contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="contact">
-    <h1>Contact</h1>
+    <div style="height: 100vh; display: flex; justify-content: center; align-items: center">
+      <div style="height: 9.5rem">
+        <h1 class="is-size-1 title">
+          <a class="has-text-dark" href="mailto:James Middleton<hello@jamesmiddleton.me>">
+            hello@jamesmiddleton.me
+          </a>
+        </h1>
+      </div>
+    </div>
   </div>
 </template>
