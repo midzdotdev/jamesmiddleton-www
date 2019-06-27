9408977ab3f892e883351241f4bbe412bc9a58e5
james2mid <jamesmiddleton1@hotmail.co.uk>
2019-06-27 15:32:53 +0100

    Prepared project for Bulma

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Buefy from 'buefy'
+
+import './sass/index.sass'
+
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
