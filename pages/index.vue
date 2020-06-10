4cbd259cf121a22a06e9ce585f6c300e3c54e792
James Middleton <hello@jamesmiddleton.me>
2020-06-10 16:46:55 +0100

    refactor: Migrate index to tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="home" class="container">
-    <section class="section">
-      <p class="title is-size-3 subtitle">I'm James.</p>
-      <p class="title is-size-1 title has-text-black-ter">
-        I'm
+  <div class="container">
+    <p class="text-3xl font-medium">I'm James.</p>
+    <p class="-mt-2 text-6xl font-bold text-gray-800">
+      I'm
 
-        <transition name="text-transition" mode="out-in">
-          <span :key="determiner">
-            {{ determiner }}
-          </span>
-        </transition>
+      <transition
+        enter-active-class="transition-all duration-150 ease-in"
+        leave-active-class="transition-all duration-200 ease-out"
+        enter-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-class="opacity-100"
+        leave-to-class="opacity-0"
+        mode="out-in"
+      >
+        <span :key="determiner">
+          {{ determiner }}
+        </span>
+      </transition>
 
-        <transition name="text-transition" mode="out-in" appear>
-          <span :key="noun">
-            {{ noun }}.
-          </span>
-        </transition>
-      </p>
+      <transition
+        enter-active-class="transition-all duration-150 ease-in"
+        leave-active-class="transition-all duration-200 ease-in"
+        enter-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-class="opacity-100"
+        leave-to-class="opacity-0"
+        mode="out-in"
+      >
+        <span :key="noun">
+          {{ noun }}.
+        </span>
+      </transition>
+    </p>
 
-      <div class="buttons">
-        <nuxt-link to="/portfolio" class="button is-outlined is-dark">VIEW MY WORK</nuxt-link>
-        <nuxt-link to="/contact" class="button is-outlined is-dark">HIRE ME</nuxt-link>
-      </div>
-    </section>
+    <div class="flex mt-2">
+      <nuxt-link to="/portfolio" class="px-4 py-2 mr-3 text-black border-2 border-black hover:bg-gray-900 hover:text-white">VIEW MY WORK</nuxt-link>
+      <nuxt-link to="/contact" class="px-4 py-2 mr-3 text-black border-2 border-black hover:bg-gray-900 hover:text-white">HIRE ME</nuxt-link>
+    </div>
   </div>
 </template>
 
