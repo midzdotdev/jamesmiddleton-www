d3d4687988b7ba266fca76cec8de7d6ce8f368d7
James Middleton <hello@jamesmiddleton.me>
2020-06-12 13:09:50 +0100

    chore: Add purgecss as nuxt build module

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,7 @@ export default {
     '@nuxt/typescript-build',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
+    'nuxt-purgecss',
   ],
   /*
   ** Nuxt.js modules
