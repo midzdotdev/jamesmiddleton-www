d746b21051ed8e0b2ed6037d387f91777832f1fc
James Middleton <accounts@jamesmiddleton.me>
2021-07-13 09:57:40 +0100

    feat: init new nextjs tailwindcss app

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+
+const Home: React.FC = () => {
+  return <p className="m-3 bg-gray-200">This is the home page</p>
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-/*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
 module.exports = {
-  theme: {},
-  variants: {},
-  plugins: []
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
